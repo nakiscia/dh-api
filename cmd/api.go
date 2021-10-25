dea51a04e0555b4db7527fafe346fc9701826aa8
Ahmet Nakisci <ahmet.nakisci@modanisa.com>
2021-10-25 22:54:32 +0300

    read port from env variables for heroku

--- cmd/api.go
+++ cmd/api.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"dh-api/server"
+	"os"
 	"time"
 )
 
 func main() {
-
-	s:= server.NewServer(&server.Config{
-		Hostname:            "",
-		Port:                "8181",
-		PersistenceFilePath: "./key-record.txt",
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8181"
+	}
+	s := server.NewServer(&server.Config{
+		Hostname:                 "",
+		Port:                     port,
+		PersistenceFilePath:      "./key-record.txt",
 		PersistenceWriteDuration: time.Duration(1000),
 	})
 
